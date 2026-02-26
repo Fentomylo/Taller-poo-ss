7458ad8f8c43a9286bc91ee869bb06222defa734
tomas velez duque <tomyvelezd@gmail.com>
2026-02-25 21:22:18 -0500

    sdadas

--- GestionEmpleados/obj/Debug/net10.0/GestionEmpleados.AssemblyInfo.cs
+++ GestionEmpleados/obj/Debug/net10.0/GestionEmpleados.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionEmpleados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9564f652b281636c8ef72550ec69ed78ff3ec6eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9df321a5e16a9d292f5e28cee28758eda18e978")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionEmpleados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionEmpleados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
